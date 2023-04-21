928afc40ebbe79887a9b763c61bee61b9cc70472
Scott Solkhon <scottsolkhon@gmail.com>
2023-04-21 18:31:03 +0100

    Move logging to file

--- chatty/logger.py
+++ chatty/logger.py
@@ -1,24 +1,28 @@
 import logging
+import os
 
 
 class Logger:
-    def __init__(self, name, level=logging.INFO):
+    def __init__(self, name, level=logging.INFO, filename="chatty.log"):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(level)
 
-        # create console handler and set level to debug
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(level)
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
+
+        # create console handler and set logging level
+        file_handler = logging.FileHandler(f"logs/{ filename }")
+        file_handler.setLevel(level)
 
         # create formatter
         formatter = logging.Formatter('%(asctime)s - %(name)s - %('
                                       'levelname)s - %(message)s')
 
-        # add formatter to console_handler
-        console_handler.setFormatter(formatter)
+        # add formatter to handlers
+        file_handler.setFormatter(formatter)
 
-        # add console_handler to logger
-        self.logger.addHandler(console_handler)
+        # add handlers to logger
+        self.logger.addHandler(file_handler)
 
     def info(self, message):
         self.logger.info(message)
